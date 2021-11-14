62fc37e7aaab936502e8a2d4d02167122da1a44a
Feliciano07 <fernandoxhajon122@gmail.com>
2021-11-13 21:11:34 -0600

    respuesta para los reportes de nodejs

--- server/routes/index.routes.js
+++ server/routes/index.routes.js
@@ -7,13 +7,43 @@ router.get('/', (req = request, res = response)=>{
     res.send("Server corriendo")
 });
 
+// REPORTES DE MONGODB
 
-router.get('/listaJuegos', (req = request, res = response)=>{
-
+/*
+ - Reporte Datos almacenados
+ - Tabla con los logs almacenados
+*/
+router.get('/juegos', (req = request, res = response)=>{
     Juegos.find()
         .exec()
         .then(x => res.status(200).send(x));
+});
 
+/**
+ * Grafica del top 3 juegos
+ */
+router.get('/top-juegos', (req= request, res = response)=>{
+    Juegos.aggregate([
+        {
+            $sortByCount: '$gamename'
+        }
+    ])
+    .limit(3)
+    .exec()
+    .then(x => res.status(200).send(x));
+});
+
+/**
+ * Grafica que compara a los 3 workes
+ */
+router.get('/worker', (req = request, res = response)=>{
+    Juegos.aggregate([
+        {
+            $sortByCount: '$worker'
+        }
+    ])
+    .exec()
+    .then(x => res.status(200).send(x));
 })
 
 module.exports = router;
